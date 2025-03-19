e979fb6a6c496126b5d5c422d73c62cb187b41e3
Siddharth2207 <siddharthbhoite22@gmail.com>
2025-03-19 22:48:35 +0530

    debug

--- src/routes/[tokenSlug]-[network]/+layout.ts
+++ src/routes/[tokenSlug]-[network]/+layout.ts
@@ -1,30 +1,20 @@
 import { tokenSlug, network, activeSubgraphs, settings } from '$lib/stores/report';
 import { networkBlockTime } from '$lib/constants';
 import { type NetworkConfigSourceWithBlockTime } from '$lib/types';
+import { get } from 'svelte/store';
+import { ConfigSource } from '@rainlanguage/orderbook/js_api';
 export const load = async ({ fetch, params }) => {
-	const response = await fetch(
-		'https://raw.githubusercontent.com/rainlanguage/rain.strategies/refs/heads/main/settings.json'
-	);
-	const settingsJson = await response.json();
+	const settingsJson: ConfigSource | undefined = get(settings);
 
-	// Add block time to each network
-	settingsJson.networks = Object.fromEntries(
-		Object.entries(settingsJson.networks).map((network: [string, unknown]) => {
-			const networkConfig: NetworkConfigSourceWithBlockTime =
-				network[1] as NetworkConfigSourceWithBlockTime;
-			networkConfig.blockTime = networkBlockTime[networkConfig['chain-id']];
-			return [network[0], networkConfig];
-		})
-	);
-
+	if (settingsJson && settingsJson.subgraphs) {
+		activeSubgraphs.set([{ url: settingsJson.subgraphs[params.network], name: params.network }]);
+	}
 	settings.set(settingsJson);
 	tokenSlug.set(params.tokenSlug.toUpperCase());
 	network.set(params.network);
-	activeSubgraphs.set([{ url: settingsJson.subgraphs[params.network], name: params.network }]);
 
 	return {
 		stores: {
-			settings,
 			tokenSlug,
 			network,
 			activeSubgraphs
