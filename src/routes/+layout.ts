af197e9bd3c02b906cda11a170b5188e52216395
Siddharth2207 <siddharthbhoite22@gmail.com>
2025-03-20 12:27:15 +0530

    update settings

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,7 +1,7 @@
 import { settings } from '$lib/stores/report';
 export const load = async ({ fetch }) => {
 	const response = await fetch(
-		'https://raw.githubusercontent.com/rainlanguage/rain.strategies/refs/heads/2025-03-19-update-bsc-rpc-url/settings.json'
+		'https://raw.githubusercontent.com/rainlanguage/rain.strategies/refs/heads/main/settings.json'
 	);
 	const settingsJson = await response.json();
 	settings.set(settingsJson);
