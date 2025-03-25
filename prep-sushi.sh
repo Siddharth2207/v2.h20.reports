3da35674cc11e7254083a408ccf216db2e3cb65b
Siddharth2207 <siddharthbhoite22@gmail.com>
2025-03-25 15:38:12 +0530

    debug deployment

--- prep-sushi.sh
+++ prep-sushi.sh
@@ -3,6 +3,8 @@
 set -euxo pipefail
 
 # build the sushi package from sushiswap monorepo submodule
+pnpm install -g pnpm@10.6.2
 git submodule update --init --recursive
+
 (cd lib/sushiswap && pnpm install --frozen-lockfile)
 (cd lib/sushiswap && pnpm exec turbo run build --filter=./packages/sushi) 
