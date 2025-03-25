6bee5d47ecde0c242b6a5998cd040b419031c766
Siddharth2207 <siddharthbhoite22@gmail.com>
2025-03-25 17:12:47 +0530

    debug deployment

--- prep-sushi.sh
+++ prep-sushi.sh
@@ -2,9 +2,8 @@
 
 set -euxo pipefail
 
-pnpm i -g pnpm
 pnpm -v
 git submodule update --init --recursive
 
-(cd lib/sushiswap && pnpm install --frozen-lockfile)
+(cd lib/sushiswap && pnpm install)
 (cd lib/sushiswap && pnpm exec turbo run build --filter=./packages/sushi) 
