312861c461ff33a5f507e647a87a3490ad71d7fb
rouzwelt <rouzweltt@gmail.com>
2025-04-03 19:51:59 +0000

    fix build

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,29 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import {} from '@sveltejs/adapter-vercel';
 import { defineConfig } from 'vite';
 import { loadEnv } from 'vite';
-// import path from 'path';
 
 export default defineConfig({
 	plugins: [sveltekit()],
 	optimizeDeps: {
 		include: ['sushi/router', 'sushi/currency', 'sushi/tines', 'sushi/config', 'sushi/chain']
 	},
-	// build: {
-	// 	commonjsOptions: {
-	// 		include: [/sushi\/router/, /sushi\/currency/, /sushi\/tines/, /sushi\/config/, /sushi\/chain/]
-	// 	}
-	// },
+	build: {
+		// commonjsOptions: {
+		// 	include: [/sushi\/router/, /sushi\/currency/, /sushi\/tines/, /sushi\/config/, /sushi\/chain/]
+		// },
+		rollupOptions: {
+			external: [
+				'sushi/router',
+				'sushi/currency',
+				'sushi/tines',
+				'sushi/config',
+				'sushi/chain',
+				'ethers',
+				'buffer',
+				'interval-tree-1d'
+			]
+		}
+	},
 	server: {
 		fs: {
 			// Allow serving files from the sushi package directory
