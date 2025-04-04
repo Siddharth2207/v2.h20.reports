ef722a7070ac4722146e71b6e2f1d3445663c3e0
Siddharth2207 <siddharthbhoite22@gmail.com>
2025-04-04 14:50:53 +0530

    debug

--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,41 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import {} from '@sveltejs/adapter-vercel';
 import { defineConfig } from 'vite';
-import {loadEnv} from "vite";
-import { exec } from 'child_process';
-import { promisify } from 'util';
-
-// const execAsync = promisify(exec);
-
-// function prepSushiPlugin() {
-// 	return {
-// 		name: 'prep-sushi',
-// 		// This hook runs before the build starts
-// 		async buildStart() {
-// 			try {
-// 				console.log('Running prep-sushi.sh...');
-// 				await execAsync('chmod +x ./prep-sushi.sh && ./prep-sushi.sh');
-// 				console.log('Running npm install...');
-// 				await execAsync('npm install');
-// 			} catch (error) {
-// 				console.error('Error running scripts:', error);
-// 				throw error;
-// 			}
-// 		}
-// 	};
-// }
+import { loadEnv } from 'vite';
 
 export default defineConfig({
-	plugins: [
-		// prepSushiPlugin(),
-		sveltekit()
-	],
-	define: {
-		'process.env': loadEnv('', process.cwd(),''),
-		'import.meta.vitest': 'undefined'
+	plugins: [sveltekit()],
+	optimizeDeps: {
+		include: ['sushi/router', 'sushi/currency', 'sushi/tines', 'sushi/config', 'sushi/chain']
 	},
+	build: {
+		commonjsOptions: {
+			include: [/sushi\/router/, /sushi\/currency/, /sushi\/tines/, /sushi\/config/, /sushi\/chain/]
+		},
+		rollupOptions: {
+			external: [
+				/sushi\/[a-zA-Z0-9-_]/,
+				/@ethersproject\/[a-zA-Z0-9-_]/,
+				'buffer',
+				'interval-tree-1d'
+			]
+		}
+	},
+	server: {
+		fs: {
+			// Allow serving files from the sushi package directory
+			allow: [
+				// Default directories
+				'src',
+				'node_modules',
+				'.svelte-kit',
+				// Add sushi package directory
+				'./lib'
+			]
+		}
+	},
+	define: {
+		'process.env': loadEnv('', process.cwd(), ''),
+		'import.meta.vitest': 'undefined'
+	}
 });
