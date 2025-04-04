efc46deb8d6e0803c324542de0b80de252467e2c
Siddharth2207 <siddharthbhoite22@gmail.com>
2025-04-04 14:25:58 +0530

    debug deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,41 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import {} from '@sveltejs/adapter-vercel'
+import {} from '@sveltejs/adapter-vercel';
 import { defineConfig } from 'vite';
-import {loadEnv} from "vite";
+import { loadEnv } from 'vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
-	define: {
-		'process.env': loadEnv('', process.cwd(),''),
-		'import.meta.vitest': 'undefined'
+	optimizeDeps: {
+		include: ['sushi/router', 'sushi/currency', 'sushi/tines', 'sushi/config', 'sushi/chain']
 	},
+	build: {
+		commonjsOptions: {
+			include: [/sushi\/router/, /sushi\/currency/, /sushi\/tines/, /sushi\/config/, /sushi\/chain/]
+		},
+		rollupOptions: {
+			external: [
+				/sushi\/[a-zA-Z0-9-_]/,
+				/@ethersproject\/[a-zA-Z0-9-_]/,
+				'buffer',
+				'interval-tree-1d'
+			]
+		}
+	},
+	server: {
+		fs: {
+			// Allow serving files from the sushi package directory
+			allow: [
+				// Default directories
+				'src',
+				'node_modules',
+				'.svelte-kit',
+				// Add sushi package directory
+				'./lib'
+			]
+		}
+	},
+	define: {
+		'process.env': loadEnv('', process.cwd(), ''),
+		'import.meta.vitest': 'undefined'
+	}
 });
